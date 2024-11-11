1df330fb16ef1e4a277d3405e8861d4120301a6b
Fernando <frr4212@gmail.com>
2024-11-12 00:26:36 +0100

    portada de cardenalito actualizada, ontap agregado a los libros

--- lib/data/bookdata.dart
+++ lib/data/bookdata.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/material.dart';
+
 class BookData {
   String bookCover;
   String bookName;
   String bookGenre;
   String bookpageNum;
   String bookDescription;
+  String bookContent;
 
   BookData(
       this.bookCover,
@@ -11,7 +14,32 @@ class BookData {
       this.bookGenre,
       this.bookDescription,
       this.bookpageNum,
+      [this.bookContent='']
       );
+  void openBook()
+  {
+    @override
+    Widget build(BuildContext context) {
+      return GestureDetector( 
+        child: Container(
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.blueAccent,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('hola'),
+              SizedBox(height: 5),
+              Text('muy buenas'),
+            ],
+          ),
+        ),
+      );
+    }
+  }
 }
 
 
